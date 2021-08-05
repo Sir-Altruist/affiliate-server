8615138513b28970dc30e488bd50b7f0079f3662
Sir-Altruist <eshooluwasegun98@gmail.com>
2021-08-05 06:54:37 +0100

    nothing new

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -7,12 +7,12 @@ const dotenv = require('dotenv').config()
 //google oauth
 const oAuth2Client = new OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI)
 oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN })
-const accessToken = await oAuth2Client.getAccessToken() 
 
 
 
 const sendEmail = async (email, subject, text) => {
     try {
+        const accessToken = await oAuth2Client.getAccessToken() 
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
